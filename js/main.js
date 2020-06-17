5be02d6d6686af001d87356057d7d2249a7725b0
lobna Ibrahem <30407102+lobnaibrahem@users.noreply.github.com>
2020-06-17 23:08:58 +0200

    add homepage box
    1. add typed text
    2. style with css
    3.typed function in js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true,}
+    );
