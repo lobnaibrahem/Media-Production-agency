2fd61e19c6db94aca3f6c08ae4ed390bbcf2f869
lobna Ibrahem <30407102+lobnaibrahem@users.noreply.github.com>
2020-06-22 00:27:45 +0200

    add some changes
    1-add funtion for the navbar change while scrolling
    2- add jumptron box in home page
    3 - change carousel images to background
    4-add comments to the code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 50,
-    backSpeed: 30,
-    backDelay: 1000,
-    startDelay: 1000,
-    loop: true,}
-    );
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-      }
-      
-      function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-      }
+// navbar scroll function
+var navChange = $(".test").offset().top
+$(window).scroll(function(){
+
+ var windowScroll = $(window).scrollTop()
+ 
+ if(windowScroll > navChange)
+     {
+         
+$(".custom-nav").css("backgroundColor","#f8f8f8")
+$(".custom-nav").css("boxShadow","5px 5px 15px 0 rgba(31, 31, 31, 0.07)")
+$(".custom-nav").css("height","60px")        
+$(".nav-item a").css("color","#000")
+$(".icons a i").css("color","#000")
+$(".icons").css("borderLeft","1px solid #000")
+
+         
+     }
+else
+    {
+$(".custom-nav").css("backgroundColor","transparent") 
+$(".custom-nav").css("boxShadow","0")
+$(".custom-nav").css("height","80px")
+$(".icons").css("borderLeft","none")
+$(".nav-item a").css("color","#f8f8f8")
+$(".icons a i").css("color","#f8f8f8")
+    }
+   
+})
+
+// var typed = new Typed('#typed', {
+//     stringsElement: '#typed-strings',
+//     typeSpeed: 50,
+//     backSpeed: 30,
+//     backDelay: 1000,
+//     startDelay: 1000,
+//     loop: true,}
+//     );
